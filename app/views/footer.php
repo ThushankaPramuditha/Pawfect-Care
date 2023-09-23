dc61c23408852f89e302e995d8b93789a5e4bee6
ThushankaPramuditha <thushankapramuditha17@gmail.com>
2023-09-23 17:53:53 +0530

    new commits

--- app/views/footer.php
+++ app/views/footer.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pawfect Care - Footer</title>
+    <title>Pawfect Care -Footer</title>
     <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>

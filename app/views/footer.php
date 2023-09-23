acc120d73a58577c0436044104e1e52215b2df97
Thushanka_Pramuditha <123655891+ThushankaPramuditha@users.noreply.github.com>
2023-09-23 17:13:44 +0530

    Add files via upload
    added app and public file

---
+++ app/views/footer.php
@@ -0,0 +1,11 @@
+<div class="footer">
+    <div class="footer-logo">
+        <img src="/assets/images/Logo 2.png" alt="Footer Logo">
+        Pawfect Care
+    </div>
+    <ul class="footer-links">
+        <li><a href="/privacy">Privacy Policy</a></li>
+        <li><a href="/terms">Terms of Service</a></li>
+        <li><a href="/contact">Contact Us</a></li>
+    </ul>
+</div>
---
+++ app/views/navbar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pawfect Care</title>
+    <link rel="stylesheet" href="assets/css/navbar.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="logo">
+            <img src="assets/images/logo 1.png" alt="Pawfect Care Logo">
+        </div>
+        <ul class="nav-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/services">Services</a></li>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/contact">Contact Us</a></li>
+            <li><a href="/login">Login</a></li>
+        </ul>
+    </div>
+    <!-- Your webpage content goes here -->
+</body>
+</html>

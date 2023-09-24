9204a67449b1dc14319be073128de217cf7bd11a
ThushankaPramuditha <thushankapramuditha17@gmail.com>
2023-09-24 18:08:40 +0530

    petdetailstable added

---
+++ app/components/tables/petdetailstable.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/tables.css">
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Pet ID</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Breed</th>
+                <th>Weight</th>
+                <th></th>
+                <th></th>
+                
+            </tr>
+        </thead>
+        <tbody>
+         
+            <tr>
+                <td>123456</td>
+                <td>Roxy</td>
+                <td>2 years</td>
+                <td>Labrador</td>
+                <td>20kg</td>
+
+                <td><button class="t-button">Medical History</button></td>
+                <td><button class="t-button">Vaccination History</button></td>
+            </tr>
+        
+        </tbody>
+    </table>
+</body>
+</html>

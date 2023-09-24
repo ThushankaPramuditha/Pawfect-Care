35a24f5bd2d1f0a2a93959e618a29725edcc2730
ThushankaPramuditha <thushankapramuditha17@gmail.com>
2023-09-24 13:28:10 +0530

    feedback.php added

---
+++ app/components/tables/feedback.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table Example</title>
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/FAQtables.css">
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Feedback</th>
+                <th>Answer</th>
+                <th>Post</th>
+                <th class="edit-action-buttons">Edit</th>
+                <th class="delete-action-buttons">Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Example data rows -->
+            <tr>
+                <td>Good Service</td>
+                <td>Thank you very much.</td>
+                <td><button class="t-button">Post</button></td>
+                <td class="edit-action-buttons">
+                    <button class="edit-icon"></button>
+                </td>
+                <td class="delete-action-buttons">
+                    <button class="delete-icon"></button>
+                </td>
+            </tr>
+            <!-- Add more rows as needed -->
+        </tbody>
+    </table>
+</body>
+</html>

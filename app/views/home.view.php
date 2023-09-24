104cb982a54d7e4390ffb105c743d96a4adf87ab
ThushankaPramuditha <thushankapramuditha17@gmail.com>
2023-09-24 13:04:06 +0530

    tables component added

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pawfect Care - Home</title>
-    <link rel="stylesheet" href="assets/css/navbar.css"> 
-    <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>
     <?php include 'navbar.php'; ?> 
+    <?php include 'tables.php'; ?>
     <?php include 'footer.php'; ?>
     <!-- Your webpage content goes here -->
 </body>

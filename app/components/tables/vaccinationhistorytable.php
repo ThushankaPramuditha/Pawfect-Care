1d8dffa075f3b66464c8a28e21a7e18e20bb0fef
ThushankaPramuditha <thushankapramuditha17@gmail.com>
2023-09-24 22:47:01 +0530

    ambulance,medicalhistory,vaccinationhistory added

---
+++ app/components/tables/vaccinationhistorytable.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/tables.css">
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Vaccine</th>
+                <th>Serial Number</th>
+                <th>Administered By</th>
+                <th>Next Due Date</th>
+               
+            </tr>
+        </thead>
+        <tbody>
+         
+            <tr>
+                <td>24/09/2023</td>
+                <td>Parvo</td>
+                <td>vac-123456</td>
+                <td>Dr.Mihiraj Magamage</td>
+                <td>24/09/2024</td>
+            </tr>
+
+            <tr>
+                <td>21/09/2023</td>
+                <td>Distemper</td>
+                <td>vac-127856</td>
+                <td>Dr.Mihiraj Magamage</td>
+                <td>21/09/2024</td>
+            </tr>
+        
+        </tbody>
+    </table>
+</body>
+</html>

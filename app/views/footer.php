5c1c78687222ff97033a89392101c494734443e9
ThushankaPramuditha <thushankapramuditha17@gmail.com>
2023-09-24 10:48:56 +0530

    new commit

--- app/views/footer.php
+++ app/views/footer.php
@@ -2,22 +2,26 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
     <title>Pawfect Care -Footer</title>
     <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>
-   
+
 <div class="footer">
-    <div class="footer-logo">
-        <img src="/assets/images/Logo 2.png" alt="Footer Logo"> 
+<div class="footer-logo">
+        <img src="assets/images/footer-logo.png" alt="Footer Logo">
     </div>
     <ul class="footer-links">
         <li><a href="/privacy">Privacy Policy</a></li>
         <li><a href="/terms">Terms of Service</a></li>
         <li><a href="/contact">Contact Us</a></li>
     </ul>
+   
 </div>
+
  <!-- Your webpage content goes here -->
 </body>
 </html>
+
+

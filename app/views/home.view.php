c048c1bbd9f25ed8724ba83b163b63d5ddc70693
ThushankaPramuditha <thushankapramuditha17@gmail.com>
2023-09-27 14:38:50 +0530

    new commits

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pawfect Care - Home</title>
 </head>
+
 <body>
-    <?php include 'navbar.php'; ?> 
-    <?php include 'addpetownerform.php'; ?> 
+    <?php include 'navbar.php'; ?>  
     <?php include 'footer.php'; ?>
 </body>
 </html>

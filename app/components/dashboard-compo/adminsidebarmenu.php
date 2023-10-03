0bd348bfed6fd111ab3717821bb1270817374e3f
Shashikala Munasinghe <shashi14854@gmail.com>
2023-10-03 07:59:01 +0530

    updated adminsidebarmenu.php

--- app/components/dashboard-compo/adminsidebarmenu.php
+++ app/components/dashboard-compo/adminsidebarmenu.php
@@ -6,19 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="assets/css/admindashboard.css">
-
+    <link rel="stylesheet" href="admindashboard.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 
 </head>
 
 <body>
     <nav>
-        <div class="logo-name">
-            <div class="logo-image">
-                <img src="assets/images/Logo 1.png" alt="">
-            </div>
+        <div class="logo-image">
+            <i class="uil uil-bars sidebar-toggle"></i>
+            <img src="Logo 1.png" alt="">
         </div>
-
+       
         <div class="menu-items">
             <ul class="nav-links">
                 <li><a href="#">
@@ -54,7 +53,8 @@
             </ul>
 
             <ul class="logout-mode">
-                <li><a href="#">
+                <li><a href="logout.php">
+                    <i class="uil uil-signout"></i>
                         <span class="link-name">Logout</span>
                     </a>
                 </li>
@@ -62,7 +62,8 @@
         </div>
     </nav>
 
-    <script src="assets/js/admindashboard.js"></script>
+    <section class="overlay"></section>
+    <script src="admindashboard.js"></script>
 
 </body>
 

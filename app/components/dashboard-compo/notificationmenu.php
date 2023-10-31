8c3a93201fe888822fb232f2fa689e0a6c973862
Shashikala Munasinghe <shashi14854@gmail.com>
2023-10-31 18:56:49 +0530

    notificationmenu added,css code in style tag,notification.css not needed

---
+++ app/components/dashboard-compo/notificationmenu.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <style>
+
+/* .notificationmenu {
+    position: relative;
+    display: grid;
+    min-height: 500px;
+    background: #c1c3c4;
+    width: 300px;
+    align-items: center;
+}
+
+.cardHeader {
+    font-size: 20px;
+    background: #fff;
+    color: #000;
+    padding: 10px;
+    border-radius: 6px;
+    display: flex;
+    justify-content: center;
+}
+
+.cardHeader h2 {
+    font-size: 20px;
+    font-weight: 100;
+    color: #000;
+    margin: 0; /* Remove the margin to eliminate space */
+
+/* .message {
+    position: relative;
+    overflow: hidden;
+    display: block;
+    border-radius: 20px;
+    border-color: #000;
+    height: fit-content;
+    width: 10px;
+    padding: 15px 140px;
+    background-color: #f5f4f4;
+    transition: all 0.5s ease;
+    margin: 0.2em;;
+}
+
+.message:hover {
+    background: #8167de;
+    color: #fff;
+}
+.message.text{
+    font-size: 16px;
+    font-weight: 50;
+    text-align: cenr;
+    margin:0;
+} */ 
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+}
+
+.notificationmenu {
+    position: relative;
+    display: grid;
+    min-height: 500px;
+    background: #c1c3c4;
+    width: 300px;
+    align-items: center;
+    padding: 10px;
+    border-radius: 20px;
+}
+
+.cardHeader {
+    font-size: 20px;
+    background: #c1c3c4;
+    color: #000;
+    border-radius: 6px;
+    display: flex;
+    justify-content: center;
+    padding: 10px;
+}
+
+.message {
+    position: relative;
+    border-radius: 12px;
+    border: 1px;
+    overflow: hidden;
+    padding: 15px;
+    background-color: #f5f4f4;
+    transition: 0.5s;
+    height:fit-content;
+}
+
+.message:hover {
+    background: #8167de;
+    color: #fff;
+}
+
+.text {
+    font-size: 16px;
+    font-weight: 500;
+}
+
+.message h3 {
+    font-size: 16px;
+    font-weight: 500;
+    text-align: center;
+}
+
+/* To align the second line of text in the "text" span */
+.message .text span {
+    display: block;
+}
+
+    </style>
+</head>
+<body>
+      
+        <div class="notificationmenu">
+                <div class="cardHeader">
+                    <h2>NOTIFICATIONS</h2>
+                 </div>
+                 <div class="message">
+                    <h3>Danniel Radicliff-Q&A<br><span>Bdciuwdj;Abogjig</span></h3>
+                 </div>
+                 <div class="message">
+                    <h3>Emma Watson-Feedback<br><span>Bdciuwdj;Abogjig</span></h3>
+                 </div>
+                 <div class="message">
+                    <h3>Roxy-Feedback<br><span>Bdciuwdj;Abogjig</span></h3>
+                 </div>
+                 <div class="message">
+                    <h3>Roxy-Q&A<br><span>Bdciuwdj;Abogjig</span></h3>
+                 </div>
+        </div>
+    
+</body>
+</html>
+

636fed674f1b03b6f3bce0a3c6bc3288cf082d49
Amandi Udawatta <amandiudawatta@gmail.com>
2023-11-01 05:46:58 +0530

    linking added - admin

--- app/views/components/tables/drivertable.php
+++ app/views/components/tables/drivertable.php
@@ -30,7 +30,7 @@
                 <td>johndoe@example.com</td>
                 <td>DR123456</td>
                 <td class="edit-action-buttons">
-                    <button class="edit-icon"></button>
+                <a href = "<?php echo $_SESSION['updatepath'] ?>"><button class="edit-icon"></button></a>
                 </td>
                 <td class="deactivate-action-buttons">
                     <button class="deactivate-button">Deactivate</button>
@@ -45,7 +45,7 @@
                 <td>janesmith@example.com</td>
                 <td>DR678901</td>
                 <td class="edit-action-buttons">
-                    <button class="edit-icon"></button>
+                <a href = "<?php echo $_SESSION['updatepath'] ?>"><button class="edit-icon"></button></a>
                 </td>
                 <td class="deactivate-action-buttons">
                     <button class="deactivate-button">Deactivate</button>

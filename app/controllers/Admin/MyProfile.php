64a7125518a89d2449be8714548c2a739f1efeb0
Amandi Udawatta <amandiudawatta@gmail.com>
2024-04-15 11:17:17 +0530

    admin my profile done

--- app/controllers/Admin/MyProfile.php
+++ app/controllers/Admin/MyProfile.php
@@ -9,8 +9,15 @@ class MyProfile
 
 	public function index()
 	{
+		//get id and gmail form session and store in user object
+		$data['user'] = new stdClass();
+		$data['user']->email = $_SESSION['USER']->email;
+		$data['user']->id = $_SESSION['USER']->id;
 
 		$data['username'] = empty($_SESSION['USER']) ? 'User':$_SESSION['USER']->email;
+
+
+		
 
 		$this->view('admin/myprofile',$data);
 	}
--- app/views/admin/myprofile.view.php
+++ app/views/admin/myprofile.view.php
@@ -13,16 +13,10 @@
     <div style = "margin-left: 230px; padding: 10px 10px 100px 100px;">
         <?php include '../app/views/components/panel-header-bar/hiuser.php'; ?>
                 <h1>My Profile</h1> 
-                <div class= "pair"><div class = "key">Staff ID:</div>  <div class = "value">12345</div></div>
-                <div class= "pair"><div class = "key">Full Name:</div> <div class = "value">John Doe</div></div>
-                <div class= "pair"><div class = "key">Address:</div> <div class = "value">Hospital Road, Dodangoda, Kalutara</div></div>
-                <div class= "pair"><div class = "key">Contact No:</div> <div class = "value">077-4441482</div></div>
-                <div class= "pair"><div class = "key">NIC:</div> <div class = "value">20018420398</div></div>
-                <div class= "pair"><div class = "key">Email:</div> <div class = "value">johndoe@gmail.com</div></div>
-                <div class= "pair"><div class = "key">Qualifications:</div> <div class = "value">DVM, PhD</div></div>
+                <div class= "pair"><div class = "key">Staff ID:</div>  <div class = "value"><?= htmlspecialchars($user->id) ?></div></div>
+                <div class= "pair"><div class = "key">Email:</div> <div class = "value"><?= htmlspecialchars($user->email) ?></div></div>
 
                 <div class="flex-container">
-                        <button type="submit" id="edit-button"><a href="editprofile">Edit Profile</a></button>
                         <button type="submit" id="changepw-button"><a href ="changepassword">Change Password</a></button>
                 </div>
             

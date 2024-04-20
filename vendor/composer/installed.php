cec2515908148f47d4d613e78a3652d9c833d961
Thushanka_Pramuditha <123655891+ThushankaPramuditha@users.noreply.github.com>
2024-04-20 11:29:56 +0000

    new commits

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-Thushanka-dev',
+        'version' => 'dev-Thushanka-dev',
+        'reference' => 'da0538ad0136f53362d901c0c4c5434d2f6d8aa0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-Thushanka-dev',
+            'version' => 'dev-Thushanka-dev',
+            'reference' => 'da0538ad0136f53362d901c0c4c5434d2f6d8aa0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.0',
+            'version' => '6.9.0.0',
+            'reference' => '934f852bfa68e59476023626461fed0c8ed6d052',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

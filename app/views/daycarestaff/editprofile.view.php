8f80d8bb84581bdb93206b36f8264708a64410a4
Amandi Udawatta <amandiudawatta@gmail.com>
2024-04-22 01:53:57 +0530

    daycare  panel and searchbar fixed: only frontend

--- app/views/daycarestaff/editprofile.view.php
+++ app/views/daycarestaff/editprofile.view.php
@@ -15,10 +15,10 @@
 </head>
 
 <body>
-
+<?php include '../app/views/components/panel-header-bar/hiuser.php'; ?>
+<div style = "margin-top: 80px; ">
 <?php include '../app/views/components/dashboard-compo/daycaresidebar.php'; ?>   
     <div style = "margin-left: 230px; padding: 10px 10px 100px 100px;">
-        <?php include '../app/views/components/panel-header-bar/hiuser.php'; ?>
         <h1>Edit Profile</h1> 
         <div class = "formcontainer" id="updatedaycarestaff"> 
             <form id="edit-profile-form" action="<?php echo ROOT?>/Daycarestaff/EditProfile/update/<?php echo $userdata->id; ?>" method="post">
@@ -45,7 +45,7 @@
             </form>   
         
     </div>
-                
+      </div>          
     </div>
 
 </body>

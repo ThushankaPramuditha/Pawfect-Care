49decc6f51c24bbad9a79195771e7f3acef82225
Amandi Udawatta <amandiudawatta@gmail.com>
2024-04-22 02:52:26 +0530

    daycare booking report added

--- app/controllers/Admin/Reports.php
+++ app/controllers/Admin/Reports.php
@@ -9,7 +9,7 @@ class Reports {
     public function index(){
         $this->view('admin/reports');
     }
-
+////////////////////////////////////////////////////////////////appointment_report////////////////////////////////////////////////////////////////
     public function generateAppointmentReport() {
         $from = $_POST['from'];
         $to = $_POST['to'];
@@ -106,4 +106,76 @@ class Reports {
         }
         return $vetTables;
     }
+
+
+
+    ////////////////////////////////////////////////////////////////daycare_report////////////////////////////////////////////////////////////////
+    public function generateDaycareReport() {
+        $from = $_POST['from'];
+        $to = $_POST['to'];
+
+        $bookingModel = new DaycarebookinguserModel();
+        $bookingCount = $bookingModel->countAllDaycareBookings($from, $to);
+        $declinedCount = $bookingModel->countDaycareBookingsByStatus('declined', $from, $to);
+        //$finishedCount = $bookingModel->countDaycareBookingsByStatus('finished', $from, $to);
+
+        $html = $this->prepareHTMLReportDaycareBooking($bookingCount, $declinedCount, $from, $to);
+        // echo $html;
+        // exit;
+
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream("Daycare Booking report {$from} to {$to}.pdf", ["Attachment" => false]);
+    }
+
+    private function prepareHTMLReportDaycareBooking($bookingCount, $declinedCount, $from, $to) {
+        $reportDate = date('Y-m-d');
+        $logoPath = ROOT . '/assets/images/footer-logo.png'; // Ensure the path to your logo is correct
+        $html = <<<HTML
+        <html>
+        <head>
+            <style>
+                body { font-family: 'Helvetica', sans-serif; font-size: 14px; padding: 20px; }
+                header { text-align: center; margin-bottom: 20px; }
+                h1 { text-align: center; font-size: 24px; color: #333; text-decoration:underline; }
+                h3 { font-weight: normal; margin-top: 0px; }
+                table { width: 100%; border-collapse: collapse; margin-bottom: 30px; }
+                th, td { border: 1px solid #ddd; padding: 10px; text-align: left; font-size: 16px; }
+                th { background-color: #f8f8f8; }
+                
+            </style>
+        </head>
+        <body>
+            <h1>Daycare Booking Report</h1>
+            <br><br>
+     
+            <h3><b>Period:</b> {$from} to {$to}</h3>
+            <h2>Total Bookings</h2>
+
+            
+            <table>
+                <thead>
+                    <tr>
+                        <th>Measure</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>Total Daycare Booking</td><td>{$bookingCount}</td></tr>
+                    <tr><td>Declined Bookings</td><td>{$declinedCount}</td></tr>
+                </tbody>
+            </table>
+            
+            <footer>
+                <p>This report was generated on {$reportDate}.</p>
+            </footer>
+        </body>
+        </html>
+        HTML;
+        return $html;
+    }
+
+
 }
--- app/models/DaycarebookingModel.php
+++ app/models/DaycarebookingModel.php
@@ -83,4 +83,5 @@ class DaycarebookingModel
 
         return empty($this->errors);
     }
+   
 }
